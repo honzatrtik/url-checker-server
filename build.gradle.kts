3e62fcefc71bd535ca05de2cb45c1c0f9ac0554c
Jan Trtík <jan.trtik@bonami.cz>
2020-05-16 16:03:42 +0200

    Init project using spring boot starter

---
+++ build.gradle.kts
@@ -0,0 +1,55 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.3.0.RELEASE"
+	id("io.spring.dependency-management") version "1.0.9.RELEASE"
+	kotlin("jvm") version "1.3.72"
+	kotlin("plugin.spring") version "1.3.72"
+	kotlin("kapt") version "1.3.72"
+}
+
+group = "urlchecker"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+repositories {
+	mavenCentral()
+
+	maven("https://dl.bintray.com/arrow-kt/arrow-kt/")
+	maven("https://maven.pkg.github.com/honzatrtik/url-checker-scala")
+}
+
+val arrowVersion = "0.10.4"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
+
+	implementation("io.arrow-kt:arrow-core:$arrowVersion")
+	implementation("io.arrow-kt:arrow-syntax:$arrowVersion")
+
+	kapt("io.arrow-kt:arrow-meta:$arrowVersion")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test") {
+		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+	}
+	testImplementation("io.projectreactor:reactor-test")
+
+
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "1.8"
+	}
+}
